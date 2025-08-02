04008284ac9d4b0390338e197cd0b7187b2b8104
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 18:27:56 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/primeversion/main.go
@@ -0,0 +1,23 @@
+// cmd/primeversion/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"primeversion/internal/primeversion"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := primeversion.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
